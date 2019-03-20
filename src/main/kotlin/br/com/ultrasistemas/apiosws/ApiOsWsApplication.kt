977c0122b89bd18c26677e40fd3e656474d190aa
LuizFelipehh <lfheemann@hotmail.com>
2019-03-19 22:35:33 -0300

    Alteração no pom.xml

--- src/main/kotlin/br/com/ultrasistemas/apiosws/ApiOsWsApplication.kt
+++ src/main/kotlin/br/com/ultrasistemas/apiosws/ApiOsWsApplication.kt
@@ -1,9 +1,15 @@
 package br.com.ultrasistemas.apiosws
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("br.com.ultrasistemas.apiosws")
+@EntityScan("br.com.ultrasistemas.apiosws")
 class ApiOsWsApplication
 
 fun main(args: Array<String>) {
